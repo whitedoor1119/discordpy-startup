6a039cf446e7c3d384bc708581684f3d883d9259
whitedoor1119 <50481928+whitedoor1119@users.noreply.github.com>
2019-09-02 13:36:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,14 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 client = discord.Client()
-
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
